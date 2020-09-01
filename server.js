3e68f35f84b57626e5c6b64e3e34e5df8df59b75
mdwohl <mdwohl@gmail.com>
2020-09-01 14:10:47 -0700

    home route test; typos fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //Packages
 const express = require('express');
 const cors = require('cors');
-const superagent = require('superagent');
+// const superagent = require('superagent');
 const pg = require('pg');
 require('dotenv').config();
 
@@ -13,10 +13,10 @@ const app = express();
 const methodOverride = require('method-override');
 const client = new pg.Client()//database url here;
 client.on('error', error => console.error(error));
-const OMDB_API_KEY = process.env.OMDB_API_KEY;
+// const OMDB_API_KEY = process.env.OMDB_API_KEY;
 //pass in object argument from movieObject
-const movieSearchUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_API_KEY}`;
-const posterSearchUrl = `http://img.omdbapi.com/?i=tt3896198&h=600&apikey=${OMDB_APRI_KEY}`;
+// const movieSearchUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_API_KEY}`;
+// const posterSearchUrl = `http://img.omdbapi.com/?i=tt3896198&h=600&apikey=${OMDB_API_KEY}`;
 
 
 
@@ -28,8 +28,12 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(methodOverride('_method'));
-app.get('/api/movies/:id',) //function of getSingleMovie)
-app.get('/', (req, res) => res.render('pages/index'));
+// app.get('/api/movies/:id', getSingleMovie);
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+// res.render('pages/index'));
 
 //Server
 client.connect()
@@ -38,16 +42,16 @@ client.connect()
   });
 
 //Routes
-app.get('/api/movies', (req, res));
-app.get('api/movies/:id', (req, res));
-app.post('/api/movies', (req, res));
-app.delete('/api/movies/:id', (req, res));
+// app.get('/api/movies', (req, res));
+// app.get('api/movies/:id', (req, res));
+// app.post('/api/movies', (req, res));
+// app.delete('/api/movies/:id', (req, res));
 
 //Functions
-function handleError(error, response) {
-  console.error(error);
-  response.render('error', {error});
-}
+// function handleError(error, res) {
+//   console.error(error);
+//   res.render('error', {error});
+// }
 
 const movieObject = [ {id:1,title:2 } ];
 
